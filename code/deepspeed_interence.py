b564a70db95be9af13e09f956fce83461d8e5a7c
zhangchong <zhangchong@xiaoduotech.com>
2023-05-31 15:27:35 +0800

    test deepspeed inference add tokenizer

--- code/deepspeed_interence.py
+++ code/deepspeed_interence.py
@@ -10,11 +10,11 @@ from transformers import pipeline
 sys.path.append(os.path.normpath(f'{os.path.dirname(os.path.abspath(__file__))}/..'))
 logger = logging.getLogger(__name__)
 
-def main(prompt, model_name_or_path, world_size=8):
+def main(prompt, model_name_or_path, tokenizer_name_or_path, world_size=8):
     print('================>')
     local_rank = int(os.getenv('LOCAL_RANK', '0'))
     world_size = int(os.getenv('WORLD_SIZE', world_size))
-    generator = pipeline('text-generation', model=model_name_or_path,
+    generator = pipeline('text-generation', model=model_name_or_path, tokenizer=tokenizer_name_or_path,
                         device=local_rank)
 
     generator.model = deepspeed.init_inference(generator.model,
@@ -26,6 +26,7 @@ def main(prompt, model_name_or_path, world_size=8):
     print(f'================>{string}')
     if not torch.distributed.is_initialized() or torch.distributed.get_rank() == 0:
         print(string)
+
 
 
 if __name__ == "__main__":
