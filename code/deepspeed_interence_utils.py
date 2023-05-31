dad5fb1d86529520638c3b9c015d9a545e356821
zhangchong <zhangchong@xiaoduotech.com>
2023-05-31 16:40:52 +0800

    test official deep inference

--- code/deepspeed_interence_utils.py
+++ code/deepspeed_interence_utils.py
@@ -1,6 +1,6 @@
-'''
+"""
 Helper classes and functions for examples
-'''
+"""
 
 import os
 import io
@@ -9,21 +9,29 @@ import json
 import deepspeed
 import torch
 from huggingface_hub import snapshot_download
-from transformers import AutoConfig, AutoModelForCausalLM, AutoTokenizer, LlamaTokenizerFast
+from transformers import (
+    AutoConfig,
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    LlamaTokenizerFast,
+)
 
-class DSPipeline():
-    '''
+
+class DSPipeline:
+    """
     Example helper class for comprehending DeepSpeed Meta Tensors, meant to mimic HF pipelines.
     The DSPipeline can run with and without meta tensors.
-    '''
-    def __init__(self,
-                 model_name='bigscience/bloom-3b',
-                 tokenizer_name='',
-                 dtype=torch.float16,
-                 is_meta=True,
-                 device=-1,
-                 checkpoint_path=None
-                 ):
+    """
+
+    def __init__(
+        self,
+        model_name="bigscience/bloom-3b",
+        tokenizer_name="",
+        dtype=torch.float16,
+        is_meta=True,
+        device=-1,
+        checkpoint_path=None,
+    ):
         self.model_name = model_name
         self.dtype = dtype
 
@@ -37,13 +45,18 @@ class DSPipeline():
             self.device = torch.device(f"cuda:{device}")
 
         # the Deepspeed team made these so it's super fast to load (~1 minute), rather than wait 10-20min loading time.
-        self.tp_presharded_models = ["microsoft/bloom-deepspeed-inference-int8", "microsoft/bloom-deepspeed-inference-fp16"]
+        self.tp_presharded_models = [
+            "microsoft/bloom-deepspeed-inference-int8",
+            "microsoft/bloom-deepspeed-inference-fp16",
+        ]
 
-        self.tokenizer = AutoTokenizer.from_pretrained(tokenizer_name, padding_side="left")
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            tokenizer_name, padding_side="left"
+        )
         self.tokenizer.pad_token = self.tokenizer.eos_token
 
-        if (is_meta):
-            '''When meta tensors enabled, use checkpoints'''
+        if is_meta and checkpoint_path:
+            """When meta tensors enabled, use checkpoints"""
             self.config = AutoConfig.from_pretrained(self.model_name)
             self.repo_root, self.checkpoints_json = self._generate_json(checkpoint_path)
 
@@ -54,55 +67,58 @@ class DSPipeline():
 
         self.model.eval()
 
-
-    def __call__(self,
-                inputs=["test"],
-                num_tokens=100,
-                do_sample=False):
+    def __call__(self, inputs=["test"], num_tokens=100, do_sample=False):
         if isinstance(inputs, str):
             input_list = [inputs]
         else:
             input_list = inputs
 
-        outputs = self.generate_outputs(input_list, num_tokens=num_tokens, do_sample=do_sample)
+        outputs = self.generate_outputs(
+            input_list, num_tokens=num_tokens, do_sample=do_sample
+        )
         return outputs
-
 
     def _generate_json(self, checkpoint_path=None):
         if checkpoint_path is None:
-            repo_root = snapshot_download(self.model_name,
-                                      allow_patterns=["*"],
-                                      cache_dir=os.getenv("TRANSFORMERS_CACHE", None),
-                                      ignore_patterns=["*.safetensors"],
-                                      local_files_only=False,
-                                      revision=None)
+            repo_root = snapshot_download(
+                self.model_name,
+                allow_patterns=["*"],
+                cache_dir=os.getenv("TRANSFORMERS_CACHE", None),
+                ignore_patterns=["*.safetensors"],
+                local_files_only=False,
+                revision=None,
+            )
         else:
-            assert os.path.exists(checkpoint_path), f"Checkpoint path {checkpoint_path} does not exist"
+            assert os.path.exists(
+                checkpoint_path
+            ), f"Checkpoint path {checkpoint_path} does not exist"
             repo_root = checkpoint_path
 
         if os.path.exists(os.path.join(repo_root, "ds_inference_config.json")):
             checkpoints_json = os.path.join(repo_root, "ds_inference_config.json")
-        elif (self.model_name in self.tp_presharded_models):
+        elif self.model_name in self.tp_presharded_models:
             # tp presharded repos come with their own checkpoints config file
             checkpoints_json = os.path.join(repo_root, "ds_inference_config.json")
         else:
             checkpoints_json = "checkpoints.json"
 
             with io.open(checkpoints_json, "w", encoding="utf-8") as f:
-                file_list = [str(entry).split('/')[-1] for entry in Path(repo_root).rglob("*.[bp][it][n]") if entry.is_file()]
+                file_list = [
+                    str(entry).split("/")[-1]
+                    for entry in Path(repo_root).rglob("*.[bp][it][n]")
+                    if entry.is_file()
+                ]
                 data = {"type": "BLOOM", "checkpoints": file_list, "version": 1.0}
                 json.dump(data, f)
 
         return repo_root, checkpoints_json
 
-
-    def generate_outputs(self,
-                         inputs=["test"],
-                         num_tokens=100,
-                         do_sample=False):
+    def generate_outputs(self, inputs=["test"], num_tokens=100, do_sample=False):
         generate_kwargs = dict(max_new_tokens=num_tokens, do_sample=do_sample)
 
-        input_tokens = self.tokenizer.batch_encode_plus(inputs, return_tensors="pt", padding=True)
+        input_tokens = self.tokenizer.batch_encode_plus(
+            inputs, return_tensors="pt", padding=True
+        )
         for t in input_tokens:
             if torch.is_tensor(input_tokens[t]):
                 input_tokens[t] = input_tokens[t].to(self.device)
