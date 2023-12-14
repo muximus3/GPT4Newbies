412ff18ffcbb7912d4473b69f2e8f525d22b03d9
muximus <muximus10@gmail.com>
2023-12-14 10:18:35 +0800

    add bs_finder print

--- code/clm_deepspeed_no_trainer.py
+++ code/clm_deepspeed_no_trainer.py
@@ -296,9 +296,9 @@ def train(accelerator, config: TrainArgs):
                 scheduler.step()
                 optimizer.zero_grad()
             # noise scale 
-            bsfinder.on_backward_end(accelerator.unwrap_model(model), step)
+            status = bsfinder.on_backward_end(accelerator.unwrap_model(model), step)
             if (step + 1) % config.print_loss_every == 0:
-                accelerator.print(f"Epoch:{epoch}, step:{step}, loss:{train_loss.compute()}")
+                accelerator.print(f"Epoch:{epoch}, step:{step}, loss:{train_loss.compute()}, bsfinder status:{status}")
                 if config.wandb:
                     accelerator.log({"train_loss": train_loss.compute()}, step=curr_step)
 
